Backpropagation error corrected in model

scripts/lstm/generate_forecast.py
@@ -21,10 +21,14 @@ config = {
 'seismic_csv': 'gs://us-geomechanicsforco2-dev-staging/temporal_datasets/cushing_2014_oklahoma/seismic.csv',
 # Pressure data
 'pressure_csv': 'gs://us-geomechanicsforco2-dev-staging/temporal_datasets/cushing_2014_oklahoma/pressure.csv',
-# Features of interest
+# Independent variables of interest
 'feature_names': ['pressure', 'dpdt'],
+# PyTorch manual seed
+'seed': 0,
 # Fraction of full data kept aside as training data
-'train_frac': 0.665,
+'train_frac': 0.64,
+# Fraction of full data set aside as validation data
+'val_frac': 0.195,
 # Sequence length
 'seq_length': 16,
 # Batch size
@@ -36,7 +40,7 @@ config = {
 # Dropout probability
 'dropout': 0.0,
 # Monotonic activation function
-'monotonic_fn': lambda x : x ** 2,
+'monotonic_fn': F.relu,
 # Learning rate
 'lr': 1.0e-5,
 # Loss criterion
@@ -53,6 +57,7 @@ config = {
 }
 ####################################################################
 def fit_lstm(config: dict) -> None:
+    torch.manual_seed(config['seed'])
     # Read in data.
     seismic_df = pd.read_csv(config['seismic_csv'])
     pressure_df = pd.read_csv(config['pressure_csv'])

scripts/lstm/hyperparam_tuning.py
@@ -20,8 +20,8 @@ _FUNCS = {
     'abs' : lambda x : x.abs(),
     'quad' : lambda x : x ** 2,
     'relu' : F.relu,
+    'sigmoid': torch.sigmoid,
     'exp' : torch.exp,
-    'sigmoid' : torch.sigmoid,
     'identity' : lambda x : x
 }
 

src/saif/lstm/model.py
@@ -47,8 +47,8 @@ class ShallowRegLSTM(nn.Module):
         # Shape of hn = (num_layers, batch size, hidden_size)
         out = self.linear(hn.permute(1,0,2).flatten(start_dim=1, end_dim=-1))
         # Enforce monotonicity.
-        out = self.monotonic_fn(out)
+        out = self.monotonic_fn(out.clone())
         # Force initial value of the horizon to be lower bounded by the final value of the input sequence.
         # Assumes the last feature to be the cumulative count of seismic events.
-        out += x[:, -1, -1, None]
+        out = out.clone() + x.clone()[:, -1, -1, None]
         return out